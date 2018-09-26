f3b2680b6e193cb8a3001b07f0f74ad34ed9c7a8
dielduarte <magdiel.kesller@gmail.com>
2018-09-26 09:17:01 +0200

    chore: first public version

---
+++ index.js
@@ -0,0 +1,13 @@
+import memoize from 'fast-memoize';
+
+function getClass(styles, conditionalStyles) {
+  return Object.keys(conditionalStyles).reduce((prev, next) => {
+    if (conditionalStyles[next]) {
+      return [...prev, styles[next]];
+    }
+
+    return prev;
+  }, []);
+}
+
+export default memoize(getClass);
